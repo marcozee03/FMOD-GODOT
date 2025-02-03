3bd2347f25ac4b468368e352f26e3dc3ca66a152
Marco Zepeda <123316474+marcozee03@users.noreply.github.com>
2025-02-03 01:31:37 -0500

    slight improvement to export script

--- export.py
+++ export.py
@@ -19,21 +19,21 @@ def main():
     parser = argparse.ArgumentParser(prog= 'ExportFmodGodot', description = 'builds FmodGodot gdextension for platform and can export addon to a folder')
     parser.add_argument('-o', '--output', type=Path, help= "the destination folder")
     parser.add_argument('-p', '--platform', type=str, help= "the platform to build for")
-
-
     args, unknown = parser.parse_known_args(sys.argv)
     platform = args.platform
-    if platform == None and sys.platform.startswith('linux') :
-        platform = "linux"
-    if platform == None and sys.platform.startswith('win'):    
-        platform = "windows"
-    if platform == "all":
-        for platform in supported_platforms:
+    
+    if not sys.argv.__contains__('-sc') :
+        if platform == None and sys.platform.startswith('linux') :
+            platform = "linux"
+        if platform == None and sys.platform.startswith('win'):    
+            platform = "windows"
+        if platform == "all":
+            for platform in supported_platforms:
+                buildForPlatform(platform)
+                exportDependencies(platform)
+        else :
             buildForPlatform(platform)
             exportDependencies(platform)
-    else :
-        buildForPlatform(platform)
-        exportDependencies(platform)
     if args.output.is_dir():
         print("\"" + str(args.output) + "\" does not exist or is not a directory")
         return;
@@ -58,4 +58,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main();
+    main()
