4de0f072b372964d39f8a322ac00973283c35885
Marco Zepeda <123316474+marcozee03@users.noreply.github.com>
2026-05-18 23:37:51 -0400

    merged include folder with src folder

---
+++ src/register_types.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <core/class_db.hpp>
+
+using namespace godot;
+
+void initialize_fmod_module(ModuleInitializationLevel p_level);
+void uninitialize_fmod_module(ModuleInitializationLevel p_level);
