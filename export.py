2d873dda6f5a62807e3efe3545ca630cc13eddf2
Marco Zepeda <123316474+marcozee03@users.noreply.github.com>
2026-05-21 01:17:11 -0400

    Refactored Export Script and Pushed Fmod Libraries into action

--- export.py
+++ export.py
@@ -6,9 +6,11 @@ import subprocess
 import sys
 import platform
 from pathlib import Path
+from fmod_installer import get_os
 
 supported_platforms = ["linux", "windows"]
 
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
 def buildForPlatform(platform, arch):
     p1 = subprocess.run(
@@ -54,63 +56,83 @@ def buildForPlatform(platform, arch):
         print("failed to build for release")
         exit(1)
 
-
 def exportDependencies(platform, arch):
-    for file in os.listdir("libs/%s/%s" % (platform, arch)):
+    os.makedirs(os.path.join(script_dir, "plugin_template", "bin", platform, arch),exist_ok=True)
+    for file in os.listdir(os.path.join(script_dir, "libs", platform, arch)):
         shutil.copy(
-            "libs/%s/%s/%s" % (platform, arch, file),
-            "plugin_template/bin/%s/%s/%s" % (platform, arch, file),
+            os.path.join(script_dir,"libs", platform, arch, file),
+            os.path.join(script_dir,"plugin_template","bin", platform, arch, file)
         )
-    shutil.copy("fmod_installer.py","plugin_template")
-
+    shutil.copy(os.path.join(script_dir,"fmod_installer.py"),os.path.join(script_dir,"plugin_template"))
 
 def main():
     parser = argparse.ArgumentParser(
         prog="ExportFmodGodot",
         description="builds FmodGodot gdextension for platform and can export addon to a folder",
     )
-    parser.add_argument("-o", "--output", type=Path, help="the destination folder")
-    parser.add_argument("-p", "--platform", type=str, help="the platform to build for")
-    parser.add_argument(
+    subparsers = parser.add_subparsers(dest="command", help="Descriptions", required=True, description="Subcommand to run")
+    build = subparsers.add_parser(name= "build",help="Build Plugin", description="Builds the plugins and places it in plugin_templates")
+    export = subparsers.add_parser(name= "export",help="Export plugin", description="Copies the contents of plugin_template to destination. Likely <path>/<to>/<project>/addons")
+    push = subparsers.add_parser(name= "push",help="Push Dependencies", description="Copies necessary files ie the fmod libs to plugin_template/")
+    export.add_argument("destination", type=Path, help="the destination folder")
+    export.add_argument("-f", "--force",type=bool, help="Overwrite files in destination")
+    build.add_argument("-p", "--platform", type=str, help="the platform to build for")
+    build.add_argument(
         "-a", "--architecture", type=str, help="the cpu architecture to build for"
     )
-    args, unknown = parser.parse_known_args(sys.argv)
+    
+    push.add_argument("-p", "--platform", type=str, help="the platform to build for")
+    push.add_argument(
+        "-a", "--architecture", type=str, help="the cpu architecture to build for"
+    )
+    args = parser.parse_args()
+
     os_platform = args.platform
+    if os_platform == None:
+        os_platform = get_os()
     arch = platform.machine()
     if arch == "AMD64":
         arch = "x86_64"
     if args.architecture != None:
         arch = args.architecture
-    if not sys.argv.__contains__("-sc"):
-        if os_platform == None and sys.platform.startswith("linux"):
-            os_platform = "linux"
-        if os_platform == None and sys.platform.startswith("win"):
-            os_platform = "windows"
-        if os_platform == "all":
-            for os_platform in supported_platforms:
+    match args.command:
+        case "build":
+            if os_platform == "all":
+                for os_platform in supported_platforms:
+                    buildForPlatform(os_platform, arch)
+                    exportDependencies(os_platform, arch)
+            else:
                 buildForPlatform(os_platform, arch)
                 exportDependencies(os_platform, arch)
-        else:
-            buildForPlatform(os_platform, arch)
-            exportDependencies(os_platform, arch)
-    if not args.output.is_dir():
-        print('"' + str(args.output) + '" does not exist or is not a directory')
-        return
-
-    if args.output != None:
-        path = Path(args.output)
-        pathA = Path(str(path).removesuffix('"')).joinpath("FmodGodot")
-        if pathA.exists():
-            if not sys.argv.__contains__("-f"):
-                print(str(pathA) + " already exists overrwrite ? (y/n)")
-                input_string = input()
-                if input_string.lower() == "y\n" or input_string.lower() == "yes\n":
-                    shutil.copytree("plugin_template", pathA, dirs_exist_ok=True)
-                else:
-                    return
-            shutil.copytree("plugin_template", pathA, dirs_exist_ok=True)
-        else:
-            shutil.copytree("plugin_template", pathA)
+        case "export":
+            if args.destination == None or not args.destination.is_dir():
+                print('"' + str(args.output) + '" does not exist or is not a directory')
+                return;
+            path = Path(args.output)
+            pathA = Path(str(path).removesuffix('"')).joinpath("FmodGodot")
+            force = args.force
+            if args.force == None:
+                force = False
+            if pathA.exists():
+                if not force:
+                    print(str(pathA) + " already exists overrwrite ? (y/n)")
+                    input_string = input()
+                    if input_string.lower() == "y\n" or input_string.lower() == "yes\n":
+                        shutil.copytree("plugin_template", pathA, dirs_exist_ok=True)
+                    else:
+                        return
+                shutil.copytree("plugin_template", pathA, dirs_exist_ok=True)
+            else:
+                shutil.copytree("plugin_template", pathA)
+        case "push":
+            if os_platform == "all":
+                for os_platform in supported_platforms:
+                    buildForPlatform(os_platform, arch)
+                    exportDependencies(os_platform, arch)
+            else:
+                exportDependencies(os_platform, arch)
+        case _:
+            exit(1)
 
 
 if __name__ == "__main__":
