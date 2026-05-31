e7ead2f179a130ea3d37c966c4b98c5180950e59
Marco Zepeda <123316474+marcozee03@users.noreply.github.com>
2026-05-30 22:02:37 -0400

    adding some verbose print statements and fixed an incorrect function call on bank

--- src/scene/fmod_bank_loader.h
+++ src/scene/fmod_bank_loader.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <godot_cpp/classes/node.hpp>
 #include "fmod_bank.h"
+#include "templates/local_vector.hpp"
 using namespace godot;
 namespace FmodGodot
 {
@@ -13,12 +14,12 @@ namespace FmodGodot
         static void _bind_methods();
 
     public:
-        TypedArray<Ref<FmodBank>> banks;
+        LocalVector<Ref<FmodBank>> banks;
         FmodBankLoader();
         ~FmodBankLoader();
         void _enter_tree() override;
-        void set_banks(TypedArray<Ref<FmodBank>> banks);
-        TypedArray<Ref<FmodBank>> get_banks();
+        void set_banks(TypedArray<FmodBank> banks);
+        TypedArray<FmodBank> get_banks();
         void set_preload_sample_data(bool p_preload);
         bool is_preload_sample_data();
     };
