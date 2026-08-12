aa97bd220563e7ddd985063f1fe0c9bf76a275ab
Marco Zepeda <123316474+marcozee03@users.noreply.github.com>
2026-08-12 13:21:10 -0400

    Export plugin for exporting FmodPlugins with the project

---
+++ src/editor/plugins/fmod_export_plugin.h
@@ -0,0 +1,13 @@
+#include "classes/editor_export_plugin.hpp"
+using namespace godot;
+namespace FmodGodot
+{
+class FmodExportPlugin : public EditorExportPlugin
+{
+  private:
+  public:
+    void _export_begin(const PackedStringArray &p_features, bool p_is_debug, const String &p_path,
+                       uint32_t p_flags) override;
+    String _get_name() const override;
+};
+} // namespace FmodGodot
