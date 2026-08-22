f3067ba1472b26b3ea0886753ddacb0b8130aa91
Marco Zepeda <123316474+marcozee03@users.noreply.github.com>
2026-08-22 13:20:40 -0400

    Add UI for labeled parameters

--- src/binding/studio/parameter_cache.h
+++ src/binding/studio/parameter_cache.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "binding/conversions.h"
+#include "fmod_studio_common.h"
 #include "variant/vector2i.hpp"
 #include <fmod_studio.h>
 #include <godot_cpp/templates/vector.hpp>
@@ -18,7 +19,27 @@ struct ParameterCache
     float min_value;
     float default_value;
     float max_value;
-    bool discrete;
+    FMOD_STUDIO_PARAMETER_FLAGS flags;
+    constexpr bool is_readonly()
+    {
+        return flags & FMOD_STUDIO_PARAMETER_READONLY;
+    }
+    constexpr bool is_automatic()
+    {
+        return flags & FMOD_STUDIO_PARAMETER_AUTOMATIC;
+    }
+    constexpr bool is_global()
+    {
+        return flags & FMOD_STUDIO_PARAMETER_GLOBAL;
+    }
+    constexpr bool is_discrete()
+    {
+        return flags & FMOD_STUDIO_PARAMETER_DISCRETE;
+    }
+    constexpr bool is_labeled()
+    {
+        return flags & (FMOD_STUDIO_PARAMETER_LABELED | FMOD_STUDIO_PARAMETER_DISCRETE);
+    }
     ParameterCache()
     {
     }
@@ -27,7 +48,7 @@ struct ParameterCache
         full_path = String("param:/") + p_description.name;
         guid = cast_to_vector4i(p_description.guid);
         default_value = p_description.defaultvalue;
-        discrete = p_description.flags & FMOD_STUDIO_PARAMETER_DISCRETE;
+        flags = p_description.flags;
         min_value = p_description.minimum;
         max_value = p_description.maximum;
     }
